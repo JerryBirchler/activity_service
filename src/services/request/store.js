50b6d1053856bd90566d7bfef41ec414e5a87881
Jerry Birchler <jerry.birchler@icloud.com>
2020-06-08 22:22:44 -0500

    fix current index logic

--- src/services/request/store.js
+++ src/services/request/store.js
@@ -276,42 +276,43 @@ Store.prototype.new = async function (request) {
                     const value = properties[key];
                     key_value += value;
                     key_value += "\u0000";
-                    logger.debug(`key_value: ${key_value}`);
+                    logger.debug(`Store new key_value: ${key_value}`);
                 }
             });
 
-            logger.debug(`key_name: [${key_name}], key_value: [${key_value}]`);
-            const request_index = await Request_Index.findOneAsync({ key_name, key_value: { '$gte': key_value }}, { consistency: models.consistencies.local_quorum });
-            const before = await Request.findOneAsync({ uuid: request_index.uuid}, { consistency: models.consistencies.local_quorum });
+            logger.debug(`Store new request_index key_name: [${key_name}], key_value: [${key_value}]`);
+            const request_index = await Request_Index.findOneAsync({ key_name, key_value: { '$eq': key_value }}, { consistency: models.consistencies.local_quorum });
             if (request_index) {
-                const uuid = request_index.uuid;
-                logger.debug(`uuid: ${uuid}`);        
-                queries.push(new Request_Index({
-                    key_name: request_index.key_name,
-                    key_value: request_index.key_value,
-                    created: request_index.created,
-                    uuid: request_index.uuid
-                }).delete({ return_query: true }));
-                
                 const before = await Request.findOneAsync({ uuid: request_index.uuid}, { consistency: models.consistencies.local_quorum });
                 if (before) {
-                    const before_data = JSON.parse(before.data || "{}");
-                    if (before_data.current) {
-                        before_data.current = false;
+                    const uuid = request_index.uuid;
+                    logger.debug(`Store new uuid: ${uuid}`);                            
+                    const before = await Request.findOneAsync({ uuid: request_index.uuid}, { consistency: models.consistencies.local_quorum });
+                    if (before) {
+                        const before_data = JSON.parse(before.data || "{}");
+                        if (before_data.current) {
+                            before_data.current = false;
+                            before.data = JSON.stringify(before_data);
+                        }
+                        queries.push(new Request_Index({
+                            key_name: request_index.key_name,
+                            key_value: request_index.key_value,
+                            created: request_index.created,
+                            uuid: request_index.uuid
+                        }).delete({ return_query: true }));
+                        queries.push(new Request({
+                            uuid: before.uuid,
+                            method: before.method,
+                            url: before.url,
+                            headers: before.headers,
+                            body: before.body,
+                            data: before.data,
+                            display_message: before.display_message,
+                            state: before.state,
+                            task_id: before.task_id,
+                            created: before.created                                
+                        }).save({ return_query: true }));       
                     }
-                    before.data = JSON.stringify(before_data);
-                    queries.push(new Request({
-                        uuid: before.uuid,
-                        method: before.method,
-                        url: before.url,
-                        headers: before.headers,
-                        body: before.body,
-                        data: before.data,
-                        display_message: before.display_message,
-                        state: before.state,
-                        task_id: before.task_id,
-                        created: before.created                                
-                    }).save({ return_query: true }));       
                 }
             }
         }
